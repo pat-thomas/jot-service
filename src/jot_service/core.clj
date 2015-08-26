a123d2400cacdb601598928ae2ce109dd583825f
Pat Thomas <pthomas@relaynetwork.com>
2015-08-26 08:57:38 -0400

    Very basic webservice.

--- src/jot_service/core.clj
+++ src/jot_service/core.clj
@@ -1,6 +1,18 @@
-(ns jot-service.core)
+(ns jot-service.core
+  (:require [org.httpkit.server :as http]
+            [cheshire.core      :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn app
+  [req]
+  (def req req)
+  {:status  200
+   :headers {"Content-Type" "application/json"}
+   :body    (json/generate-string {:message "Pong!"})})
+
+(defn main
+  []
+  (http/run-server #'app {:port 8080}))
+
+(comment
+  (main)
+  )
